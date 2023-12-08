c5daa414edd754409431aaf7cec89efd87b0c413
caseymeland <145603691+caseymeland@users.noreply.github.com>
2023-12-08 11:37:37 -0700

    Website Deploy

---
+++ Javascript/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+   $("#mobile-button").click(function(){
+        $("#mobile-menu").fadeIn("slow")
+   });
+   $("#close-button").click(function(){
+        $("#mobile-menu").fadeOut("slow")
+    });
+});
